095ecb02385327736cc69add7e4d1400427dcdad
fz <natureorz@foxmail.com>
2018-05-23 14:57:44 +0800

    feign请求参数

--- src/main/java/xyz/fz/controller/HelloController.java
+++ src/main/java/xyz/fz/controller/HelloController.java
@@ -1,14 +1,17 @@
 package xyz.fz.controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
     @RequestMapping("/time")
-    public String time() {
-        return System.currentTimeMillis() + "";
+    public String time(@RequestBody Map params) {
+        return System.currentTimeMillis() + params.toString();
     }
 }
